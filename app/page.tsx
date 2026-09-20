f1e5ec391de110b830077fc6a799b1c2665d1014
Brice <dimitrimehelo@gmail.com>
2026-09-20 22:48:24 +0100

    feat: page.tsx layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,14 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <section className="grid flex-1 grid-cols-1 md:grid-cols-2">
           <div className="flex flex-col justify-center gap-6 px-8 py-24 md:px-16">
-            <h1>Brice Mehelo</h1>
-            <p>Software / ai engineer - [online pitch ]</p>
+            <h1 className="text-5xl font-light uppercase tracking-tight text-black dark:text-zinc-50 md:text-7xl">
+              Brice Mehelo
+            </h1>
+            <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
+              Software / ai engineer - [online pitch ]
+            </p>
           </div>
+          <div>{/** TODO:: AI concierge chat panel */}</div>
         </section>
       </main>
     </div>
